e653092066ddf4b9354990947edec33c7ba58c5f
CongTuetheCoder <truongtue356@gmail.com>
2025-10-14 08:43:44 +0700

    Add files via upload

--- scripts/lang/language_userRegister.js
+++ scripts/lang/language_userRegister.js
@@ -10,7 +10,7 @@ const fetchLang = (path) => {
 			for (const key in data) {
 				if (Object.prototype.hasOwnProperty.call(data, key)) {
 					const element = document.getElementById(key);
-					if (key == "language-btn" || key.indexOf("lesson") !== -1) {
+					if (key == "language-btn") {
 						element.title = data[key];
 					} else if (key == "animated") {
 						element.dataset.value = data[key];
@@ -26,26 +26,18 @@ const fetchLang = (path) => {
 };
 
 const languageBtn = document.getElementById("language-btn");
-
 languageBtn.addEventListener("click", () => {
-	const dialog = document.getElementsByClassName("lessonDialog")[0];
-	dialog.innerHTML = "";
-	dialog.style.height = "0";
-	dialog.style.padding = "0";
-
 	if (localStorage.getItem("lang") === "en") {
 		localStorage.setItem("lang", "vi");
-		fetchLang("data/lang_vi/home.json");
+		fetchLang("data/lang_vi/userRegister.json");
 	} else {
 		localStorage.setItem("lang", "en");
-		fetchLang("data/lang_en/home.json");
+		fetchLang("data/lang_en/userRegister.json");
 	}
 });
 
-setTimeout(() => {
-	if (localStorage.getItem("lang") === "en") {
-		fetchLang("data/lang_en/home.json");
-	} else {
-		fetchLang("data/lang_vi/home.json");
-	}
-}, 300);
+if (localStorage.getItem("lang") === "en") {
+	fetchLang("data/lang_en/userRegister.json");
+} else {
+	fetchLang("data/lang_vi/userRegister.json");
+}
